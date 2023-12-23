c106c1719997e8bfcb2b4589ed44f5142d26100d
chyuanhan <chyuanhan@gmail.com>
2023-12-23 21:43:24 +0800

    first commit

---
+++ components/Feed.jsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { useState, useEffect } from "react"
+
+import PromptCard from "./PromptCard"
+
+const PromptCardList = ({data,handleTagClick}) => {
+  return(
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post.id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+
+const Feed = () => {
+  const [posts, setPosts] = useState([])
+
+  // Seacrh States
+  const [searchText,setSearchText] = useState('')
+  const [searchTimeout,setSearchTimeout] = useState(null)
+  const [searchedResults,setSearchedResults] = useState([])
+
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt')
+      const data = await response.json()
+      
+      setPosts(data)
+    }
+    fetchPosts()
+  },[])
+  
+  const filterPrompt = (searchText) =>{
+    const regex = new RegExp(searchText,"i") // "i" flag for case-insensitive search
+    
+    return posts.filter((item)=>
+      regex.test(item.creator.username) ||
+      regex.test(item.tag) ||
+      regex.test(item.prompt) 
+    ) 
+  }
+  
+  const handlerSearchChange = (e) => {
+    clearTimeout(searchTimeout)
+    setSearchText(e.target.value)
+
+    setSearchTimeout(setTimeout(()=>{
+      const searchResult = filterPrompt(e.target.value)
+      setSearchedResults(searchResult)
+    },500))
+  }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName)
+
+    const searchResult = filterPrompt(tagName)
+    setSearchedResults(searchResult)
+  }
+
+  return (
+    <section className="feed">
+      <form className="relative w-4/5 flex-center">
+        <input 
+          type="text" 
+          placeholder="Serach for a tag or a username"
+          value={searchText}
+          onChange={handlerSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      {searchText ? (
+        <PromptCardList
+          data = {searchedResults}
+          handleTagClick = {handleTagClick}
+        />
+      ) : (
+        <PromptCardList
+          data = {posts}
+          handleTagClick = {handleTagClick}
+        />
+      )} 
+      
+    </section>
+  )
+}
+
+export default Feed
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,23 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@my-company/tailwind-components/**/*.js',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      boxShadow: {
+        'custom-shadow': 'inset 10px -50px 94px 0 rgba(199, 199, 199, 0.2)',
       },
+      backdropBlur: {
+        'custom-blur': 'blur(20px)', // Adjust the value as needed
+      },
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      }
     },
   },
   plugins: [],
